57faf855e6d87a8041f2a48dc745d97d4165a4ce
Vítor Ribas <bsbribas66@gmail.com>
2020-05-20 19:21:21 -0300

    fix appointment id generation by typeORM

--- src/database/migrations/1589854995149-CreateAppointments.ts
+++ src/database/migrations/1589854995149-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1589854995149
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
