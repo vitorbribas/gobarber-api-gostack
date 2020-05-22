7c56381ca63e61a86e7185ee4b531188baa7f376
Vítor Ribas <bsbribas66@gmail.com>
2020-05-21 22:38:05 -0300

    change tables ids type

--- src/database/migrations/1589854995149-CreateAppointments.ts
+++ src/database/migrations/1589854995149-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1589854995149
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
